9f894f8796181fab0b6b15b4fc9086cbc5585fcc
Cash Wu <cash@cashwu.com>
2018-06-04 09:11:22 +0800

    update Ch01 Recipe3 線程等待

--- Recipe3/Program.cs
+++ Recipe3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Recipe3
 {
@@ -6,7 +7,21 @@ namespace Recipe3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting...");
+            Thread t = new Thread(PrintNumbersWithDelay);
+            t.Start();
+            t.Join();
+            Console.WriteLine("Thread Completed");
+        }
+
+        static void PrintNumbersWithDelay()
+        {
+            Console.WriteLine("Starting...");
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                Console.WriteLine(i);
+            }
         }
     }
 }
