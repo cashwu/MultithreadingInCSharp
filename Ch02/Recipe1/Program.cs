580e7d076cb9a6ba1bb422fd0ca6c9c1b272fe67
Cash Wu <cash@cashwu.com>
2018-06-04 14:30:04 +0800

    update Ch02 Recipe1 原子操作

--- Ch02/Recipe1/Program.cs
+++ Ch02/Recipe1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Recipe1
 {
@@ -6,7 +7,89 @@ namespace Recipe1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Incorrect counter");
+
+            var c = new Counter();
+            
+            var t1 = new Thread(() => TestCounter(c));
+            var t2 = new Thread(() => TestCounter(c));
+            var t3 = new Thread(() => TestCounter(c));
+            
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+
+            Console.WriteLine($"Total count : {c.Count}");
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("Correct counter");
+            
+            var c1 = new CounterNoLock();
+            
+            t1 = new Thread(() => TestCounter(c1));
+            t2 = new Thread(() => TestCounter(c1));
+            t3 = new Thread(() => TestCounter(c1));
+            
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+
+            Console.WriteLine($"Total count : {c1.Count}");
         }
+
+        static void TestCounter(CounterBase c)
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                c.Increment();
+                c.Decrement();
+            } 
+        }
+    }
+
+    class Counter : CounterBase
+    {
+        public int Count { get; private set; }
+
+        public override void Increment()
+        {
+            Count++;
+        }
+
+        public override void Decrement()
+        {
+            Count--;
+        }
+    }
+    
+    class CounterNoLock : CounterBase
+    {
+        private int _count;
+        public int Count => _count;
+
+        public override void Increment()
+        {
+            Interlocked.Increment(ref _count);
+        }
+
+        public override void Decrement()
+        {
+            Interlocked.Decrement(ref _count);
+        }
+    }
+
+    abstract class CounterBase
+    {
+        public abstract void Increment();
+
+        public abstract void Decrement();
     }
 }
