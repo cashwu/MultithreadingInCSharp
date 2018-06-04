d954189aeee3632ae887485d0884f05903d66fe9
Cash Wu <cash@cashwu.com>
2018-06-04 14:43:37 +0800

    update Ch02 Recipe3 SemaphoreSlim

--- Ch02/Recipe3/Program.cs
+++ Ch02/Recipe3/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Threading;
 
 namespace Recipe3
 {
     class Program
     {
+        static SemaphoreSlim _semaphore = new SemaphoreSlim(4);
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i <= 6; i++)
+            {
+                string threadName = "Thread " + i;
+
+                int secondsToWait = 2 + 2 * i;
+                
+                var t = new Thread(() => AccessDatabase(threadName, secondsToWait));
+                t.Start();
+            }
+        }
+
+        static void AccessDatabase(string name, int seconds)
+        {
+            Console.WriteLine($"{name} waits to access a database");
+
+            _semaphore.Wait();
+            
+            Console.WriteLine($"{name} was granted an access to a database");
+            
+            Thread.Sleep(TimeSpan.FromSeconds(seconds));
+
+            Console.WriteLine($"{name} is completed");
+
+            _semaphore.Release();
         }
     }
 }
