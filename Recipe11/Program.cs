52131d36e63b32fb7575aba0bb8657d01ee56c92
Cash Wu <cash@cashwu.com>
2018-06-04 10:52:46 +0800

    update Ch01 Recipe11 Exception

--- Recipe11/Program.cs
+++ Recipe11/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Recipe11
 {
@@ -6,7 +7,41 @@ namespace Recipe11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var t = new Thread(FaultyThread);
+            
+            t.Start();
+            t.Join();
+
+            try
+            {
+                t = new Thread(BadFaultyThread);
+                t.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("We won't get here");
+            }
+        }
+
+        static void BadFaultyThread()
+        {
+            Console.WriteLine("Starting a bad faulty thread..");
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            throw new Exception("Boom");
+        }
+
+        static void FaultyThread()
+        {
+            try
+            {
+                Console.WriteLine("Starting a faulty thread..");
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                throw new Exception("Boom");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception handled: {e.Message}");
+            }
         }
     }
 }
