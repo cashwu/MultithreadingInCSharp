2981d1d47e298e6d87bb55d8a89480d9a8666778
Cash Wu <cash@cashwu.com>
2018-06-04 15:12:20 +0800

    update Ch02 Recipe5 ManualResetEventSlim

--- Ch02/Recipe5/Program.cs
+++ Ch02/Recipe5/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Threading;
 
 namespace Recipe5
 {
     class Program
     {
+        static ManualResetEventSlim _mainEvent = new ManualResetEventSlim(false);
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var t1 = new Thread(() => TravelThroughGates("Thread 1", 5));
+            var t2 = new Thread(() => TravelThroughGates("Thread 2", 6));
+            var t3 = new Thread(() => TravelThroughGates("Thread 3", 12));
+            
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            Thread.Sleep(TimeSpan.FromSeconds(6));
+
+            Console.WriteLine("The gates are now open");
+            
+            _mainEvent.Set();
+            
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            
+            _mainEvent.Reset();
+
+            Console.WriteLine("The gates have been closed");
+            
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+
+            Console.WriteLine($"The gates are now open for the second time");
+            
+            _mainEvent.Set();
+            
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            Console.WriteLine("The gates have been closed");
+            
+            _mainEvent.Reset();
+        }
+
+        static void TravelThroughGates(string name, int seconds)
+        {
+            Console.WriteLine($"{name} falls to sleep");
+            
+            Thread.Sleep(TimeSpan.FromSeconds(seconds));
+
+            Console.WriteLine($"{name} waits for the gates to open");
+
+            _mainEvent.Wait();
+
+            Console.WriteLine($"{name} enters the gates");
         }
     }
 }
