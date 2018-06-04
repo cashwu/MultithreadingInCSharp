7ed227b6a1e3b23cb3876085e3283fc87c192cf7
Cash Wu <cash@cashwu.com>
2018-06-04 10:11:05 +0800

    update Ch01 Recipe7 前台線程後台線程

--- Recipe7/Program.cs
+++ Recipe7/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
 
 namespace Recipe7
 {
@@ -6,7 +8,38 @@ namespace Recipe7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sampleForeground = new ThreadSample(10);
+            var sampleBackground = new ThreadSample(20);
+            
+            var threadOne = new Thread(sampleForeground.CountNumbers);
+            threadOne.Name = "ForegroundThread";
+            var threadTwo = new Thread(sampleBackground.CountNumbers);
+            threadTwo.Name = "BackgroundThread";
+            threadTwo.IsBackground = true;
+            
+            threadOne.Start();
+            threadTwo.Start();
+        }
+    }
+
+    class ThreadSample
+    {
+        private readonly int _iterations;
+
+        public ThreadSample(int iterations)
+        {
+            _iterations = iterations;
+        }
+
+        public void CountNumbers()
+        {
+            for (int i = 0; i < _iterations; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(0.5));
+                Console.WriteLine($"{Thread.CurrentThread.Name} print {i}");
+            }
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} is end");
         }
     }
 }
