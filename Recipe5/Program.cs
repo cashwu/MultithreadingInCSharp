77880cf35101fb22dbead20a5e20354da426dc92
Cash Wu <cash@cashwu.com>
2018-06-04 09:26:39 +0800

    update Ch01 Recipe5 線程狀態

--- Recipe5/Program.cs
+++ Recipe5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Recipe5
 {
@@ -6,7 +7,41 @@ namespace Recipe5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting program...");
+            Thread t = new Thread(PrintNumbersWithStatus);
+            Thread t2 = new Thread(DoNothing);
+            Console.WriteLine(t.ThreadState.ToString());
+            
+            t2.Start();
+            t.Start();
+
+            for (int i = 0; i < 30; i++)
+            {
+                Console.WriteLine(t.ThreadState);
+            }
+            
+            Thread.Sleep(TimeSpan.FromSeconds(6));
+//            t.Abort();
+            Console.WriteLine("A thread has been aborted");
+            Console.WriteLine(t.ThreadState);
+            Console.WriteLine(t2.ThreadState);
+        }
+
+        static void DoNothing()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+        }
+
+        static void PrintNumbersWithStatus()
+        {
+            Console.WriteLine("Starting...");
+            Console.WriteLine(Thread.CurrentThread.ThreadState);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                Console.WriteLine(i);
+            }
         }
     }
 }
