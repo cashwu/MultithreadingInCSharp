a3611b7e69510c4f32b61844200a74607ee653cf
Cash Wu <cash@cashwu.com>
2018-06-04 09:15:37 +0800

    update Ch01 Recipe4 終止線程

--- Recipe4/Program.cs
+++ Recipe4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Recipe4
 {
@@ -6,7 +7,22 @@ namespace Recipe4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting program...");
+            Thread t = new Thread(PrintNumbersWithDelay);
+            t.Start();
+            Thread.Sleep(TimeSpan.FromSeconds(6));
+            t.Abort();
+            Console.WriteLine("A thread has been aborted");
+        }
+
+        static void PrintNumbersWithDelay()
+        {
+            Console.WriteLine("Starting...");
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                Console.WriteLine(i);
+            }
         }
     }
 }
