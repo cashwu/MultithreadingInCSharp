274d71eab8a7db7fd16e7f1c6ea3801b250ffd66
Cash Wu <cash@cashwu.com>
2018-06-04 10:47:22 +0800

    add Ch01 Recipe11 Exception

---
+++ Recipe11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recipe11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
