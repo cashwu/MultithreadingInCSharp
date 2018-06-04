3ccde263d5fb60d0aec378e8d037a49432dfd3ef
Cash Wu <cash@cashwu.com>
2018-06-04 15:18:45 +0800

    update Ch02 Recipe6 CountDownEvent

--- Ch02/Recipe6/Program.cs
+++ Ch02/Recipe6/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Threading;
 
 namespace Recipe6
 {
     class Program
     {
+        static CountdownEvent _countdown = new CountdownEvent(2);
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting two operations");
+            
+            var t1 = new Thread(() => PerformOperation("Operation 1 is completed", 4));
+            var t2 = new Thread(() => PerformOperation("Operation 2 is completed", 8));
+            
+            t1.Start();
+            t2.Start();
+
+            _countdown.Wait();
+
+            Console.WriteLine("Both operations have been completed");
+            
+            _countdown.Dispose();
+        }
+
+        static void PerformOperation(string msg, int seconds)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(seconds));
+            Console.WriteLine(msg);
+            _countdown.Signal();
         }
     }
 }
