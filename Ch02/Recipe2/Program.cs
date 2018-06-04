16745094b64b25afc76fbd4cd94f402826d5786b
Cash Wu <cash@cashwu.com>
2018-06-04 14:37:02 +0800

    update Ch02 Recipe2 Mutex

--- Ch02/Recipe2/Program.cs
+++ Ch02/Recipe2/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Threading;
 
 namespace Recipe2
 {
     class Program
     {
+        private const string MutexName = "CSharpThreadingCookbook";
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var m = new Mutex(false, MutexName))
+            {
+                if (!m.WaitOne(TimeSpan.FromSeconds(5), false))
+                {
+                    Console.WriteLine("Second instance is running"); 
+                }
+                else
+                {
+                    Console.WriteLine("Running");
+                    Console.ReadLine();
+                    m.ReleaseMutex();
+                }
+            }
         }
     }
 }
