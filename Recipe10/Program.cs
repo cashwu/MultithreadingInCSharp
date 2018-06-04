649178422da6027f784b32dadc35f99a12f2d974
Cash Wu <cash@cashwu.com>
2018-06-04 10:46:30 +0800

    update Ch01 Recipe10 Monitor

--- Recipe10/Program.cs
+++ Recipe10/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Threading;
 
 namespace Recipe10
 {
     class Program
     {
+           
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            object lock1 = new object();
+            object lock2 = new object();
+            
+            new Thread(() => LockTooMuch(lock1, lock2)).Start();
+
+            lock (lock2)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Mointor.TryEnter allows not to get stuck, returning false a specified timeout is elapsed");
+
+                if (Monitor.TryEnter(lock1, TimeSpan.FromSeconds(5)))
+                {
+                    Console.WriteLine("Acquired a protected resource succesfully");
+                }
+                else
+                {
+                    Console.WriteLine("Timeout acquiring a resource");
+                }
+            }
+
+            new Thread(() => LockTooMuch(lock1, lock2)).Start();
+
+            lock (lock2)
+            {
+                Console.WriteLine("This will be a deadlock");
+                
+                Thread.Sleep(1000);
+                lock (lock1)
+                {
+                    Console.WriteLine("Aquired a protected resource succesfully");
+                }
+            }
+        }
+
+        static void LockTooMuch(object lock1, object lock2)
+        {
+            lock (lock1)
+            {
+                Thread.Sleep(1000);
+                lock (lock2) ;
+            }
         }
     }
 }
