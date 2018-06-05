45fdabb9c4a1e77af800fc2a02d99927e2823dca
Cash Wu <cash@cashwu.com>
2018-06-05 18:04:52 +0800

    update Ch04 Recipe05 EAP轉換為任務

--- Ch04/Recipe05/Program.cs
+++ Ch04/Recipe05/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Recipe05
 {
@@ -6,7 +9,44 @@ namespace Recipe05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tcs = new TaskCompletionSource<int>();
+
+            var worker = new BackgroundWorker();
+
+            worker.DoWork += (sender, eventArgs) =>
+            {
+                eventArgs.Result = TaskMethod("Background worker", 5); 
+            };
+            
+            worker.RunWorkerCompleted += (sender, eventArgs) =>
+            {
+                if (eventArgs.Error != null)
+                {
+                    tcs.SetException(eventArgs.Error);
+                }
+                else if (eventArgs.Cancelled)
+                {
+                    tcs.SetCanceled();
+                }
+                else
+                {
+                    tcs.SetResult((int)eventArgs.Result);
+                }
+            };
+
+            worker.RunWorkerAsync();
+
+            int result = tcs.Task.Result;
+
+            Console.WriteLine($"Result is : {result}");
+        }
+
+        static int TaskMethod(string name, int seconds)
+        {
+            Console.WriteLine($"Task {name} is running on a thread id {Thread.CurrentThread.ManagedThreadId}, " +
+                              $" Is thread pool thread : {Thread.CurrentThread.IsThreadPoolThread}");
+            Thread.Sleep(TimeSpan.FromSeconds(seconds));
+            return 42 * seconds;
         }
     }
 }
