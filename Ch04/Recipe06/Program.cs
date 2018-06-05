88a4c4428aa3dd5f540877b6221b475f1407828d
Cash Wu <cash@cashwu.com>
2018-06-05 18:20:43 +0800

    update Ch04 Recipe06 實現取消選項

--- Ch04/Recipe06/Program.cs
+++ Ch04/Recipe06/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Recipe06
 {
@@ -6,7 +8,49 @@ namespace Recipe06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cts = new CancellationTokenSource();
+            var longTask = new Task<int>(() => TaskMethod("Task 1", 10, cts.Token), cts.Token);
+            Console.WriteLine(longTask.Status);
+            cts.Cancel();
+            Console.WriteLine(longTask.Status);
+            Console.WriteLine($"First task has been cancelled before execution");
+           
+            cts = new CancellationTokenSource();
+            longTask = new Task<int>(() => TaskMethod("Task 2", 10, cts.Token), cts.Token);
+            longTask.Start();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(0.5));
+                Console.WriteLine(longTask.Status);
+            } 
+            
+            cts.Cancel();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(0.5));
+                Console.WriteLine(longTask.Status);
+            }
+
+            Console.WriteLine($"A task has been complete with result {longTask.Result}");
+        }
+
+        private static int TaskMethod(string name, int seconds, CancellationToken token)
+        {
+            Console.WriteLine($"Task {name} is running on a thread id {Thread.CurrentThread.ManagedThreadId}, " +
+                              $" Is thread pool thread : {Thread.CurrentThread.IsThreadPoolThread}");
+
+            for (int i = 0; i < seconds; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                if (token.IsCancellationRequested)
+                {
+                    return -1;
+                }
+            }
+
+            return 42 * seconds;
         }
     }
 }
