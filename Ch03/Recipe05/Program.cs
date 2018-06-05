50a8de3e0c6a1a69012019c801af0eeec64ec884
Cash Wu <cash@cashwu.com>
2018-06-05 11:15:29 +0800

    update Ch03 Recipe05 線程池使用等待事件處理器及超時

--- Ch03/Recipe05/Program.cs
+++ Ch03/Recipe05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Recipe05
 {
@@ -6,7 +7,55 @@ namespace Recipe05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RunOperations(TimeSpan.FromSeconds(5));
+            RunOperations(TimeSpan.FromSeconds(7));
+        }
+
+        static void RunOperations(TimeSpan workerOperationTimeout)
+        {
+            using (var evt = new ManualResetEvent(false))
+            using (var cts = new CancellationTokenSource())
+            {
+                Console.WriteLine("Registering timeout operations..");
+                var worker = ThreadPool.RegisterWaitForSingleObject(evt, (state, isTimeout) =>
+                    {
+                        WorkerOperationWait(cts, isTimeout);
+                    }, null, workerOperationTimeout, true);
+
+                Console.WriteLine("Starting long running operation..");
+                ThreadPool.QueueUserWorkItem(_ => WorkerOperation(cts.Token, evt));
+                
+                Thread.Sleep(workerOperationTimeout.Add(TimeSpan.FromSeconds(2)));
+                worker.Unregister(evt);
+            }
+        }
+
+        private static void WorkerOperation(CancellationToken token, ManualResetEvent evt)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    return;
+                }
+                
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+
+            evt.Set();
+        }
+
+        private static void WorkerOperationWait(CancellationTokenSource cts, bool isTimeout)
+        {
+            if (isTimeout)
+            {
+                cts.Cancel();
+                Console.WriteLine("Worker operation timed out and was canceled");
+            }
+            else
+            {
+                Console.WriteLine("Worker operation succeded");
+            }
         }
     }
 }
