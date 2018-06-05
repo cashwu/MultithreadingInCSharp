cf4f549d541a1d0f1418322cb816f01c0ff79d9c
Cash Wu <cash@cashwu.com>
2018-06-05 10:30:20 +0800

    update Ch03 Recipe02 線程池中放入異步操作

--- Ch03/Recipe02/Program.cs
+++ Ch03/Recipe02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Recipe02
 {
@@ -6,7 +7,37 @@ namespace Recipe02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int x = 1;
+            const int y = 2;
+            const string lambdaState = "lambda state 2";
+
+            ThreadPool.QueueUserWorkItem(AsyncOperation);
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            ThreadPool.QueueUserWorkItem(AsyncOperation, "async statr");
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            ThreadPool.QueueUserWorkItem(state =>
+            {
+                Console.WriteLine($"operation state : {state}");
+                Console.WriteLine($"worker thread id : {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            }, "lambda state");
+
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                Console.WriteLine($"operation state : {x + y}, {lambdaState}");
+                Console.WriteLine($"worker thread id : {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            }, "lambda state");
+            
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+        }
+
+        private static void AsyncOperation(object state)
+        {
+            Console.WriteLine($"Operation state : {state ?? "(null)"}");
+            Console.WriteLine($"Worker thread id : {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(TimeSpan.FromSeconds(2));
         }
     }
 }
