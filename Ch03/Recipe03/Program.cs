7b43fb9fd1b1f00eb4a18a98da236dcd1dfe7bec
Cash Wu <cash@cashwu.com>
2018-06-05 10:42:38 +0800

    update Ch03 Recipe03 線程池與並行

--- Ch03/Recipe03/Program.cs
+++ Ch03/Recipe03/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Recipe03
 {
@@ -6,7 +8,59 @@ namespace Recipe03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int numberOfOperation = 500;
+
+            var sw = new Stopwatch();
+            sw.Start();
+            UserThreads(numberOfOperation);
+            sw.Stop();
+            Console.WriteLine($"Execution time using thread : {sw.ElapsedMilliseconds}");
+            
+            sw.Reset();
+            sw.Start();
+            UserThreadPool(numberOfOperation);
+            sw.Stop();
+            Console.WriteLine($"Execution time using thread : {sw.ElapsedMilliseconds}");
+        }
+
+        static void UserThreads(int numberOfOperations)
+        {
+            using (var countdown = new CountdownEvent(numberOfOperations))
+            {
+                Console.WriteLine($"Scheduling work by creating threads");
+                for (int i = 0; i < numberOfOperations; i++)
+                {
+                    var thread = new Thread(() =>
+                    {
+                        Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}");
+                        Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                        countdown.Signal();
+                    });
+                    thread.Start();
+                }
+
+                countdown.Wait();
+                Console.WriteLine();
+            }
+        }
+
+        static void UserThreadPool(int numberOfOperations)
+        {
+            using (var countdown = new CountdownEvent(numberOfOperations))
+            {
+                Console.WriteLine("Starting work on a threadpool");
+                for (int i = 0; i < numberOfOperations; i++)
+                {
+                    ThreadPool.QueueUserWorkItem(_ =>
+                    {
+                        Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}");
+                        Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                        countdown.Signal();
+                    });
+                }
+                countdown.Wait();
+                Console.WriteLine();
+            }
         }
     }
 }
