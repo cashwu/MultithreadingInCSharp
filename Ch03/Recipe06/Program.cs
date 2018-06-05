7bfe616a745a900fa2eaaa62de93999cd21c7bf0
Cash Wu <cash@cashwu.com>
2018-06-05 11:21:24 +0800

    update Ch03 Recipe06 Timer

--- Ch03/Recipe06/Program.cs
+++ Ch03/Recipe06/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Threading;
 
 namespace Recipe06
 {
     class Program
     {
+        private static Timer _timer;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press 'Enter' to stop the timer..");
+            
+            DateTime start = DateTime.Now;
+            
+            _timer = new Timer(_ => TimerOperation(start), null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2)); 
+            
+            Thread.Sleep(TimeSpan.FromSeconds(6));
+
+            _timer.Change(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(4));
+
+            Console.ReadLine();
+
+            _timer.Dispose();
+        }
+
+        static void TimerOperation(DateTime start)
+        {
+            TimeSpan elapsed = DateTime.Now - start;
+            Console.WriteLine($"{elapsed.Seconds} seconds from {start} Timer thread pool thread id {Thread.CurrentThread.ManagedThreadId}");
         }
     }
 }
