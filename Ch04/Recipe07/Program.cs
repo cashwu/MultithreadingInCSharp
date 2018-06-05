d03338083f992a57f62fae3f2de9ad3c777ecfef
Cash Wu <cash@cashwu.com>
2018-06-05 18:31:21 +0800

    update Ch04 Recipe07 處理任務中的異常

--- Ch04/Recipe07/Program.cs
+++ Ch04/Recipe07/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Recipe07
 {
@@ -6,7 +8,59 @@ namespace Recipe07
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task<int> task;
+            try
+            {
+                task = Task.Run(() => TaskMethod("Task 1", 2));
+                int result = task.Result;
+                Console.WriteLine($"Result : {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception caught : {ex}");
+            }
+
+            Console.WriteLine("--------------");
+            Console.WriteLine();
+
+            try
+            {
+                task = Task.Run(() => TaskMethod("Task 2", 2));
+                int result = task.GetAwaiter().GetResult();
+                Console.WriteLine($"Result ; {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception caught : {ex}");
+            }
+            
+            Console.WriteLine("--------------");
+            Console.WriteLine();
+
+            var t1 = new Task<int>(() => TaskMethod("Task 3", 3));
+            var t2 = new Task<int>(() => TaskMethod("Task 4", 2));
+
+            var complexTask = Task.WhenAll(t1, t2);
+            var exceptionHandler = complexTask.ContinueWith(
+                t =>
+                {
+                    Console.WriteLine($"Exception caught : {t.Exception}"); 
+                },
+                TaskContinuationOptions.OnlyOnFaulted);
+            
+            t1.Start();
+            t2.Start();
+            
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+        }
+
+        static int TaskMethod(string name, int seconds)
+        {
+            Console.WriteLine($"Task {name} is running on a thread id {Thread.CurrentThread.ManagedThreadId}, " +
+                              $" Is thread pool thread {Thread.CurrentThread.IsThreadPoolThread}");
+            Thread.Sleep(TimeSpan.FromSeconds(seconds));
+            throw new Exception("Boom");
+            return 42 * seconds;
         }
     }
 }
