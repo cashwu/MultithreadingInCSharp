d9727dd36037e0fc449c1157f32d3a780a843d0e
Cash Wu <cash@cashwu.com>
2018-06-05 11:32:36 +0800

    update Ch03 Recipe07 BackgroundWorker

--- Ch03/Recipe07/Program.cs
+++ Ch03/Recipe07/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Threading;
 
 namespace Recipe07
 {
@@ -6,7 +8,70 @@ namespace Recipe07
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var bw = new BackgroundWorker();
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+            bw.DoWork += Worker_DoWork;
+            bw.ProgressChanged += Worker_ProgressChanged;
+            bw.RunWorkerCompleted += Worker_Completed;
+            
+            bw.RunWorkerAsync();
+
+            Console.WriteLine("Press C to cancel work");
+
+            do
+            {
+                if (Console.ReadKey(true).KeyChar == 'C')
+                {
+                    bw.CancelAsync();
+                }
+            } while (bw.IsBusy);
+        }
+
+        private static void Worker_Completed(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Console.WriteLine($"Completed thread pool thrad id : {Thread.CurrentThread.ManagedThreadId}");
+
+            if (e.Error != null)
+            {
+                Console.WriteLine($"Exception {e.Error.Message} has occured");
+            }
+            else if (e.Cancelled)
+            {
+                Console.WriteLine("Operation has been canceled");
+            }
+            else
+            {
+                Console.WriteLine($"The answer is {e.Result}");
+            }
+        }
+
+        private static void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Console.WriteLine($"{e.ProgressPercentage} % completed. Progress thread pool thread id : {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        private static void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Console.WriteLine($"DoWork thread pool thread id {Thread.CurrentThread.ManagedThreadId}");
+            var bw = (BackgroundWorker) sender;
+            for (int i = 0; i <= 100; i++)
+            {
+                if (bw.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+
+                if (i % 10 == 0)
+                {
+                    bw.ReportProgress(i);
+                }
+                
+                Thread.Sleep(TimeSpan.FromSeconds(0.1));
+            }
+
+            e.Result = 42;
         }
     }
 }
