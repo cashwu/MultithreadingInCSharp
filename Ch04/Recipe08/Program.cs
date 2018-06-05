ce86499be5cc9fc38d8336bbd9111271fa95957a
Cash Wu <cash@cashwu.com>
2018-06-05 18:41:50 +0800

    update Ch04 Recipe08 並行運行任務

--- Ch04/Recipe08/Program.cs
+++ Ch04/Recipe08/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Recipe08
 {
@@ -6,7 +9,47 @@ namespace Recipe08
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var firstTask = new Task<int>(() => TaskMethod("First Task", 2));
+            var secondTask = new Task<int>(() => TaskMethod("Second Task", 2));
+            var whenAllTask = Task.WhenAll(firstTask, secondTask);
+
+            whenAllTask.ContinueWith(t =>
+            {
+                Console.WriteLine($"The first answer is {t.Result[0]}, the second is {t.Result[1]}");
+            });
+
+            firstTask.Start();
+            secondTask.Start();
+            
+            Thread.Sleep(TimeSpan.FromSeconds(4));
+
+            var tasks = new List<Task<int>>();
+            for (int i = 0; i < 4; i++)
+            {
+                int counter = i;
+                var task = new Task<int>(() => TaskMethod($"Task {counter}", counter));
+                 
+                tasks.Add(task);
+                task.Start();
+            }
+
+            while (tasks.Count > 0)
+            {
+                var completedTask = Task.WhenAny(tasks).Result;
+                tasks.Remove(completedTask);
+
+                Console.WriteLine($"A task has been completed with result {completedTask.Result}");
+            }
+            
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+        }
+
+        static int TaskMethod(string name, int seconds)
+        {
+            Console.WriteLine($"Task {name} is running on a thread id {Thread.CurrentThread.ManagedThreadId}, " +
+                              $" Is thread pool thread {Thread.CurrentThread.IsThreadPoolThread}");
+            Thread.Sleep(TimeSpan.FromSeconds(seconds));
+            return 42 * seconds;
         }
     }
 }
