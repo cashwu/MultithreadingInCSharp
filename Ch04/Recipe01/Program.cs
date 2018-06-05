73fafba0103d9b0100e23b3a212938fe64b5b6bc
Cash Wu <cash@cashwu.com>
2018-06-05 16:32:08 +0800

    update Ch04 Recipe01 建立任務

--- Ch04/Recipe01/Program.cs
+++ Ch04/Recipe01/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Recipe01
 {
@@ -6,7 +8,24 @@ namespace Recipe01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var t1 = new Task(() => TaskMethod("Task 1"));
+            var t2 = new Task(() => TaskMethod("Task 2"));
+            
+            t2.Start();
+            t1.Start();
+
+            Task.Run(() => TaskMethod("Task 3"));
+
+            Task.Factory.StartNew(() => TaskMethod("Task 4"));
+            Task.Factory.StartNew(() => TaskMethod("Task 5"), TaskCreationOptions.LongRunning);
+            
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+        }
+
+        static void TaskMethod(string name)
+        {
+            Console.WriteLine($"Task {name} is running on a thread id {Thread.CurrentThread.ManagedThreadId}. " +
+                              $"Is thread pool thread : {Thread.CurrentThread.IsThreadPoolThread} ");
         }
     }
 }
