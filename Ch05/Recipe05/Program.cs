de49aa4a56ffc51f2d8222577070ebb0e5f8def6
Cash Wu <cash@cashwu.com>
2018-06-06 11:15:46 +0800

    update Ch05 Recipe05 處理異步操作中的異常

--- Ch05/Recipe05/Program.cs
+++ Ch05/Recipe05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Recipe05
 {
@@ -6,7 +7,69 @@ namespace Recipe05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task t = AsynchronousProcessing();
+            t.Wait();
+        }
+
+        static async Task AsynchronousProcessing()
+        {
+            Console.WriteLine("1. signle exception");
+
+            try
+            {
+                string result = await GetInfoAsync("Task 1", 2);
+                Console.WriteLine(result);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("2. multiple exeptions");
+
+            Task<string> t1 = GetInfoAsync("Task 1", 3);
+            Task<string> t2 = GetInfoAsync("Task 2", 2);
+
+            try
+            {
+                string[] results = await Task.WhenAll(t1, t2);
+                Console.WriteLine(results.Length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception details : {ex}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("3. multiple exceptions with aggregate exception");
+
+            t1 = GetInfoAsync("Task 1", 3);
+            t2 = GetInfoAsync("Task 2", 2);
+            Task<string[]> t3 = Task.WhenAll(t1, t2);
+
+            try
+            {
+                string[] results = await t3;
+                Console.WriteLine(results.Length);
+            }
+            catch
+            {
+                var ae = t3.Exception.Flatten();
+                var exception = ae.InnerExceptions;
+                Console.WriteLine($"Exceptions caught : {exception.Count}");
+                foreach (var ex in exception)
+                {
+                    Console.WriteLine($"Exception details : {ex}");
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        static async Task<string> GetInfoAsync(string name, int seconds)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(seconds));
+            throw new Exception($"Boom from {name}");
         }
     }
 }
