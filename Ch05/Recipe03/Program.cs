42a3b0ebabc0712c3f7bb2d700c3e6a130a241ce
Cash Wu <cash@cashwu.com>
2018-06-06 10:49:23 +0800

    update Ch05 Recipe03 對連續的異步任務使用 await

--- Ch05/Recipe03/Program.cs
+++ Ch05/Recipe03/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Recipe03
 {
@@ -6,7 +8,65 @@ namespace Recipe03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task t = AsynchronyWithTPL();
+            t.Wait();
+
+            t = AsynchronyWithAwait();
+            t.Wait();
+        }
+
+        static Task AsynchronyWithTPL()
+        {
+            var containerTask = new Task(() =>
+            {
+                Task<string> t = GetInfoAsync("TPL 1");
+                t.ContinueWith(task =>
+                    {
+                        Console.WriteLine(t.Result);
+
+                        Task<string> t2 = GetInfoAsync("TPL 2");
+                        t2.ContinueWith(innerTask => { Console.WriteLine(innerTask.Result); },
+                            TaskContinuationOptions.NotOnFaulted | TaskContinuationOptions.AttachedToParent);
+
+                        t2.ContinueWith(innterTask => { Console.WriteLine(innterTask.Exception.InnerExceptions); },
+                            TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.AttachedToParent);
+                    }, TaskContinuationOptions.NotOnFaulted | TaskContinuationOptions.AttachedToParent);
+
+                t.ContinueWith(task => { Console.WriteLine(t.Exception.InnerExceptions); },
+                    TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.AttachedToParent);
+            });
+
+            containerTask.Start();
+            return containerTask;
+        }
+
+        static async Task AsynchronyWithAwait()
+        {
+            try
+            {
+                string result = await GetInfoAsync("Async 1");
+                Console.WriteLine(result);
+                result = await GetInfoAsync("Async 2");
+                Console.WriteLine(result);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        static async Task<string> GetInfoAsync(string name)
+        {
+            Console.WriteLine($"Task {name} started !");
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            if (name == "TPL 2")
+            {
+                throw new Exception("Boom");
+            }
+
+            return $"Task {name} is running on a thread id {Thread.CurrentThread.ManagedThreadId}, " +
+                   $" Is thread pool thread : {Thread.CurrentThread.IsThreadPoolThread}";
         }
     }
 }
