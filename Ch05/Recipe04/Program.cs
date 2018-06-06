57ec86a2c8e4a01ff0d349a433d51c89808669f0
Cash Wu <cash@cashwu.com>
2018-06-06 11:01:04 +0800

    update Ch05 Recipe04 對併行執行的異步任務使用 await

--- Ch05/Recipe04/Program.cs
+++ Ch05/Recipe04/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Recipe04
 {
@@ -6,7 +8,31 @@ namespace Recipe04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task t = AsynchronousProcessing();
+            t.Wait();
+        }
+
+        static async Task AsynchronousProcessing()
+        {
+            Task<string> t1 = GetInfoAsync("Task 1", 3);
+            Task<string> t2 = GetInfoAsync("Task 2", 5);
+
+            string[] results = await Task.WhenAll(t1, t2);
+
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        static async Task<string> GetInfoAsync(string name, int seconds)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(seconds));
+
+//            await Task.Run(() => Thread.Sleep(TimeSpan.FromSeconds(seconds)));
+            
+            return $"Task {name} is running on a thread id {Thread.CurrentThread.ManagedThreadId}, " +
+                   $" Is thread pool thread : {Thread.CurrentThread.IsThreadPoolThread}";
         }
     }
 }
