fb34e53d4ff2efac6973e70b35d1ed52de479bbb
Cash Wu <cash@cashwu.com>
2018-06-06 12:21:26 +0800

    update Ch05 Recipe09 對動態類型使用 await

--- Ch05/Recipe09/Program.cs
+++ Ch05/Recipe09/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using ImpromptuInterface;
 
 namespace Recipe09
 {
@@ -6,7 +11,55 @@ namespace Recipe09
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task t = AsynchronousProcessing();
+            t.Wait();
         }
+
+        static async Task AsynchronousProcessing()
+        {
+            string result = await GetDynamicAwaitableObject(true);
+            Console.WriteLine(result);
+
+            result = await GetDynamicAwaitableObject(false);
+            Console.WriteLine(result);
+        }
+
+        static dynamic GetDynamicAwaitableObject(bool completsSynchronously)
+        {
+            dynamic result = new ExpandoObject();
+            dynamic awaiter = new ExpandoObject();
+
+            awaiter.Message = "Completed synchronously";
+            awaiter.IsCompleted = completsSynchronously;
+            awaiter.GetResult = (Func<string>) (() => awaiter.Message);
+
+            awaiter.OnCompleted = (Action<Action>) (callback =>
+            {
+                ThreadPool.QueueUserWorkItem(state =>
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                    awaiter.Message = GetInfo();
+                    callback?.Invoke();
+                });
+            });
+
+            IAwaiter<string> proxy = Impromptu.ActLike(awaiter);
+
+            result.GetAwaiter = (Func<dynamic>) (() => proxy);
+
+            return result;
+        }
+
+        static object GetInfo()
+        {
+            return $"Task is running on a thread id {Thread.CurrentThread.ManagedThreadId}, " +
+                   $" Is thread pool thread {Thread.CurrentThread.IsThreadPoolThread}";
+        }
+    }
+
+    public interface IAwaiter<T> : INotifyCompletion
+    {
+        bool IsCompleted { get; }
+        T GetResult();
     }
 }
