1a3cd8d15fc49d99f148bd44cdbdc7e9778516c0
Cash Wu <cash@cashwu.com>
2018-06-06 10:11:16 +0800

    update Ch05 Recipe02 在 lambda 表達式中使用 await

--- Ch05/Recipe02/Program.cs
+++ Ch05/Recipe02/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Recipe02
 {
@@ -6,7 +8,22 @@ namespace Recipe02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task t = AsynchronousProcessing();
+            t.Wait();
+        }
+
+        static async Task AsynchronousProcessing()
+        {
+            Func<string, Task<string>> asyncLambda = async name =>
+            {
+                await Task.Delay(TimeSpan.FromSeconds(2));
+                return $"Task {name} is running on a thread id {Thread.CurrentThread.ManagedThreadId}, " +
+                       $" Is thread pool thread : {Thread.CurrentThread.IsThreadPoolThread}";
+            };
+
+            string result = await asyncLambda("async lambda");
+
+            Console.WriteLine(result);
         }
     }
 }
