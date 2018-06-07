a1bd54903686793db03c39a9d4ef9f2e3ad86dfe
Cash Wu <cash@cashwu.com>
2018-06-07 15:44:48 +0800

    update Ch06 Recipe01 ConcurrentDictionary

--- Ch06/Recipe01/Program.cs
+++ Ch06/Recipe01/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Recipe01
 {
     class Program
     {
+        private const string Item = "Dictionary item";
+        public static string CurrentItem;
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var concurrentDictionary = new ConcurrentDictionary<int, string>();
+            var dictionary = new Dictionary<int, string>();
+
+            var sw = new Stopwatch();
+            
+            sw.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                lock (dictionary)
+                {
+                    dictionary[i] = Item;
+                } 
+            }
+            sw.Stop();
+
+            Console.WriteLine($"Writing to dictionary with a lock : {sw.Elapsed}");
+
+            sw.Restart();
+            for (int i = 0; i < 1000000; i++)
+            {
+                concurrentDictionary[i] = Item;
+            }
+            sw.Stop();
+
+            Console.WriteLine($"Writing to a concurrent dictionary : {sw.Elapsed}");
+            
+            sw.Restart();
+            for (int i = 0; i < 1000000; i++)
+            {
+                lock (dictionary)
+                {
+                    CurrentItem = dictionary[i];
+                }
+            }
+            sw.Stop();
+
+            Console.WriteLine($"Reading from dictionary with a lock : {sw.Elapsed}");
+            
+            sw.Restart();
+            for (int i = 0; i < 1000000; i++)
+            {
+                CurrentItem = concurrentDictionary[i];
+            }
+            sw.Stop();
+
+            Console.WriteLine($"Reading from a concurrent dictionary : {sw.Elapsed}");
         }
     }
 }
