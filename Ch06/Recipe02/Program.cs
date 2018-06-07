00c6e0a7c8240f0f2a3e61930630d9b2e85d74fd
Cash Wu <cash@cashwu.com>
2018-06-07 15:59:46 +0800

    update Ch06 Recipe03 ConcurrentQueue

--- Ch06/Recipe02/Program.cs
+++ Ch06/Recipe02/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Recipe02
 {
@@ -6,7 +10,73 @@ namespace Recipe02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task t = RunProgram();
+            t.Wait();
         }
+
+        static async Task RunProgram()
+        {
+            var taskQueue = new ConcurrentQueue<CustomTask>();
+            var cts = new CancellationTokenSource();
+
+            var taskSource = Task.Run(() => TaskProducer(taskQueue));
+            
+            Task[] processors = new Task[4];
+            for (int i = 1; i <= 4; i++)
+            {
+                string processorId = i.ToString();
+                processors[i - 1] = Task.Run(() =>
+                {
+                    TaskProducer(taskQueue, $"Processor {processorId}", cts.Token); 
+                });
+            }
+
+            await taskSource;
+            cts.CancelAfter(TimeSpan.FromSeconds(2));
+
+            await Task.WhenAll(processors);
+        }
+
+        private static async Task TaskProducer(ConcurrentQueue<CustomTask> queue, string name, CancellationToken token)
+        {
+            CustomTask workItem;
+            bool deQueueSuccesful;
+
+            await GetRandomDelay();
+
+            do
+            {
+                deQueueSuccesful = queue.TryDequeue(out workItem);
+                if (deQueueSuccesful)
+                {
+                    Console.WriteLine($"Task {workItem.Id} has been processed by {name}");
+                }
+
+                await GetRandomDelay();
+            } while (!token.IsCancellationRequested);
+        }
+
+        private static Task GetRandomDelay()
+        {
+            int delay = new Random(DateTime.Now.Millisecond).Next(1, 500);
+            return Task.Delay(delay);
+        }
+
+        private static async Task TaskProducer(ConcurrentQueue<CustomTask> queue)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                await Task.Delay(50);
+                var workItem = new CustomTask() { Id = i }; 
+                queue.Enqueue(workItem);
+
+                Console.WriteLine($"Task {workItem.Id} has been posted");
+            }
+        }
+    }
+
+    internal class CustomTask
+    {
+        public int Id { get; set; }
     }
 }
