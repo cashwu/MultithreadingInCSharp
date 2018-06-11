b16ee8666cb9b642774a8742fdd5c51c6ef71b0b
Cash Wu <cash@cashwu.com>
2018-06-11 13:21:11 +0800

    update Ch07 Recipe03 調整 PLINQ 查詢的參數

--- Recipe03/Program.cs
+++ Recipe03/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Xml;
 
 namespace Recipe03
 {
@@ -6,7 +11,64 @@ namespace Recipe03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var parallelQuery = from t in GetTypes().AsParallel()
+                select EmulateProcessing(t);
+
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(TimeSpan.FromSeconds(1));
+
+            try
+            {
+                parallelQuery.WithDegreeOfParallelism(Environment.ProcessorCount)
+                    .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
+                    .WithMergeOptions(ParallelMergeOptions.Default)
+                    .WithCancellation(cts.Token)
+                    .ForAll(Console.WriteLine);
+            }
+            catch (OperationCanceledException e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("----");
+                Console.WriteLine("Operation has been canceled");
+            }
+
+            Console.WriteLine("----");
+            Console.WriteLine("Unordered PLINQ query execution");
+
+            var unorderQuery = from i in ParallelEnumerable.Range(1, 30)
+                               select i;
+
+            foreach (var i in unorderQuery)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("----");
+            Console.WriteLine("Ordered PLINQ query exection");
+
+            var orderedQuery = from i in ParallelEnumerable.Range(1, 30).AsOrdered()
+                select i;
+            
+            foreach (var i in orderedQuery)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static string EmulateProcessing(string typeName)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(new Random(DateTime.Now.Millisecond).Next(250, 350)));
+            Console.WriteLine($"{typeName} type was processed on a thread id {Thread.CurrentThread.ManagedThreadId}");
+            return typeName;
+        }
+
+        static IEnumerable<string> GetTypes()
+        {
+            return from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                from type in assembly.GetExportedTypes()
+                where type.Name.StartsWith("Web")
+                orderby type.Name.Length
+                select type.Name;
         }
     }
 }
