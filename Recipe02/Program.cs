e1eb74c837c6755c881f8d2c166c29dfee4a1189
Cash Wu <cash@cashwu.com>
2018-06-11 12:45:08 +0800

    add Ch07 Recipe02 並行化 linq 查詢

---
+++ Recipe02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recipe02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
