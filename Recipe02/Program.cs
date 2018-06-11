fc6e14bbf03e0c31197fa5644417c3af4647a9b6
Cash Wu <cash@cashwu.com>
2018-06-11 13:07:52 +0800

    update Ch07 Recipe02 並行化 linq 查詢

--- Recipe02/Program.cs
+++ Recipe02/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
 
 namespace Recipe02
 {
@@ -6,7 +12,101 @@ namespace Recipe02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var query = from t in GetTypes()
+                select EmulateProcessing(t);
+
+            foreach (var typeName in query)
+            {
+                PrintInfo(typeName);
+            }
+            
+            sw.Stop();
+
+            Console.WriteLine("----");
+            Console.WriteLine("Sequential LINQ query.");
+            Console.WriteLine($"Time slapsed : {sw.Elapsed}");
+            Console.WriteLine("Press ENTER to continue...");
+            Console.ReadLine();
+            Console.Clear();
+            sw.Reset();
+            
+            sw.Start();
+            var parallelQuery = from t in ParallelEnumerable.AsParallel(GetTypes())
+                select EmulateProcessing(t);
+
+            foreach (var typeName in parallelQuery)
+            {
+                PrintInfo(typeName);
+            }
+            
+            sw.Stop();
+
+            Console.WriteLine("----");
+            Console.WriteLine("Parallel LINQ query. The results are being merged on a single thread");
+            Console.WriteLine($"Time elapsed : {sw.Elapsed}");
+            Console.WriteLine("Press ENTER to continue...");
+            Console.ReadLine();
+            Console.Clear();
+            sw.Reset();
+            
+            sw.Start();
+            parallelQuery = from t in GetTypes().AsParallel()
+                select EmulateProcessing(t);
+            
+            parallelQuery.ForAll(PrintInfo);
+            
+            sw.Stop();
+
+            Console.WriteLine("----");
+            Console.WriteLine("Parallel LINQ query. The results are begin processed in parallel");
+            Console.WriteLine($"Time elapsed : {sw.Elapsed}");
+            Console.WriteLine("Press ENTER to continue...");
+            Console.ReadLine();
+            Console.Clear();
+            sw.Reset();
+            
+            sw.Start();
+
+            query = from t in GetTypes().AsParallel().AsSequential()
+                select EmulateProcessing(t);
+
+            foreach (var typeName in query)
+            {
+                PrintInfo(typeName);
+            }
+            
+            sw.Stop();
+
+            Console.WriteLine("----");
+            Console.WriteLine("Parallel LINQ query, transformed into sequential.");
+            Console.WriteLine($"Time elapsed : {sw.Elapsed}");
+            Console.WriteLine("Press ENTER to continue...");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        static string EmulateProcessing(string typeName)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(150));
+            Console.WriteLine($"{typeName} type was processed on a thread id {Thread.CurrentThread.ManagedThreadId}");
+            return typeName;
+        }
+
+        static void PrintInfo(string typeName)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(150));
+            Console.WriteLine($"{typeName} type was printed on a thread id {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        static IEnumerable<string> GetTypes()
+        {
+            return from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                from type in assembly.GetExportedTypes()
+                where type.Name.StartsWith("Web")
+                select type.Name;
         }
     }
 }
