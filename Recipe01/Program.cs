bf8c3aaa45239c09756f6b261f8edae1c706c6cb
Cash Wu <cash@cashwu.com>
2018-06-11 12:44:07 +0800

    update Ch07 Recipe01 Parallel Class

--- Recipe01/Program.cs
+++ Recipe01/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Recipe01
 {
@@ -6,7 +9,44 @@ namespace Recipe01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Parallel.Invoke(
+                () => EmulateProcessiong("Task1"),
+                () => EmulateProcessiong("Task2"),
+                () => EmulateProcessiong("Task3")
+            );
+            
+            var cts = new CancellationTokenSource();
+
+            var result = Parallel.ForEach(
+                Enumerable.Range(1, 30),
+                new ParallelOptions
+                {
+                    CancellationToken = cts.Token,
+                    MaxDegreeOfParallelism = Environment.ProcessorCount,
+                    TaskScheduler = TaskScheduler.Default
+                },
+                (i, state) =>
+                {
+                    Console.WriteLine(i);
+                    if (i == 20)
+                    {
+                        state.Break();
+                        Console.WriteLine($"Loop is stopped : {state.IsStopped}");
+                    }
+                });
+
+            Console.WriteLine("---");
+            Console.WriteLine($"IsCompleted : {result.IsCompleted}");
+            Console.WriteLine($"Lowest break iteration : {result.LowestBreakIteration}");
+        }
+
+        static string EmulateProcessiong(string taskName)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(new Random(DateTime.Now.Millisecond).Next(250, 350)));
+
+            Console.WriteLine($"{taskName} task was processed on a thread id {Thread.CurrentThread.ManagedThreadId}");
+
+            return taskName;
         }
     }
 }
