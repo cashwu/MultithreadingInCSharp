c40a02b4b95f217a042f8122fc3a5a7a93eb33f7
Cash Wu <cash@cashwu.com>
2018-06-11 13:34:37 +0800

    update Ch07 Recipe04 處理 PLINQ 查詢中的異常

--- Recipe04/Program.cs
+++ Recipe04/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Recipe04
 {
@@ -6,7 +9,54 @@ namespace Recipe04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IEnumerable<int> numbers = Enumerable.Range(-5, 10);
+
+            var query = from number in numbers
+                select 100 / number;
+
+            try
+            {
+                foreach (var n in query)
+                {
+                    Console.WriteLine(n);
+                }
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Divide by zero");
+            }
+
+            Console.WriteLine("----");
+            Console.WriteLine("Sequential LINQ query processing");
+            Console.WriteLine();
+
+            var parallelQuery = from number in numbers.AsParallel()
+                select 100 / number;
+
+            try
+            {
+                parallelQuery.ForAll(Console.WriteLine);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divide by zero - usual exception handler");
+            }
+            catch (AggregateException e)
+            {
+                e.Flatten().Handle(ex =>
+                {
+                    if (ex is DivideByZeroException)
+                    {
+                        Console.WriteLine("Divide by zero - aggregate exception handler");
+                        return true;
+                    }
+                    
+                    return false;
+                });
+            }
+
+            Console.WriteLine("----");
+            Console.WriteLine("Parallel LINQ query processing and results merging");
         }
     }
 }
